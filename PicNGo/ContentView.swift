d1c82244e956ab549ae30980ba165cde1f034816
Jianshuo Wang <jianshuo@hotmail.com>
2026-02-22 21:39:06 +0800

    Initial Commit

---
+++ PicNGo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PicNGo
+//
+//  Created by Jianshuo Wang on 2026/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PicNGo/PicNGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicNGoApp.swift
+//  PicNGo
+//
+//  Created by Jianshuo Wang on 2026/2/22.
+//
+
+import SwiftUI
+
+@main
+struct PicNGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
